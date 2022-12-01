fe3480d69b38d89f1a0c1bbf951f7f43123792ab
Adam Macdonald <72780006+twokilohertz@users.noreply.github.com>
2022-12-01 17:10:36 +0000

    Day 1 done!

--- day1/day1.py
+++ day1/day1.py
@@ -1,1 +1,20 @@
 # Advent of Code 2022, Day 1
+
+file = open("/home/adam/devel/adventofcode2022/day1/input.txt", "r")
+buffer: str = file.read()
+file.close()
+
+split_numbers: list[str] = buffer.split("\n\n")
+elves: list[int] = []
+for numbers in split_numbers:
+    total: int = 0
+    calories: list[str] = numbers.split("\n")
+    for value in calories:
+        total = total + int(value)
+    elves.append(total)
+
+elves.sort(reverse=True)
+print("Part one: " + str(elves[0]))
+
+# part two
+print("Part two: " + str(elves[0] + elves[1] + elves[2]))
